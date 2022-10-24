67ea9a2b4df9620a3638c2add9941c5787aad1f8
SalmanLS <salmanluthfi456@gmail.com>
2022-10-24 17:04:39 +0700

    membuat method linear search

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -78,5 +78,34 @@ namespace SearchAlgorithm
                 ch = char.Parse(Console.ReadLine());
             } while ((ch == 'y') || (ch == 'Y'));
         }
+        public void LinearSearch()
+        {
+            char ch;
+            //searh the number of comparison
+            int ctr;
+            do
+            {
+                //accept the number to be searched
+                Console.Write("\n enter the element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found at position" + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\n Number of comparison: " + ctr);
+                Console.Write("\n Continue search (y/n):");
+                ch = Char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+        
     }
 }
