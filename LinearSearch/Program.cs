0c57f68411e8ebd948b4b0158e1e43e0c453bb07
SalmanLS <salmanluthfi456@gmail.com>
2022-10-24 16:08:03 +0700

    deklarasi array dan variabel int nya

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -6,5 +6,12 @@ namespace SearchAlgorithm
     class Program
     {
         //array to be searched
+        int[] arr = new int[20];
+        //number of element in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+
     }
 }
